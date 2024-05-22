90d452202aa4702b9fcbd5e3da18c47de5c7cde0
CHERV <segerator1@gmail.com>
2024-05-22 17:37:05 +0300

    8 laba

---
+++ 8/yandexmaps/src/main/java/ru/mirea/yudakovam/yandexmaps/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.yudakovam.yandexmaps;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "09c25d9b-cceb-4825-b00a-0fa448c3550e";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
